febc7df836f8f2d4d783161ea945cf66e4aaf002
Samuel Ogundare <101474209+OGlearns@users.noreply.github.com>
2023-02-01 11:03:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def sell():
 
         # check to see if they own enough of the stock to sell
 
-        owned_shares = db.execute("SELECT symbol, sum(shares) FROM history WHERE id = %s AND symbol = ?", (session["user_id"], symbol))[0]["sum(shares)"]
+        owned_shares = db.execute("SELECT symbol, sum(shares) FROM history WHERE id = ? AND symbol = ?", (session["user_id"], symbol))[0]["sum(shares)"]
 
         if not owned_shares:
             return apology("Must enter valid number of shares")
