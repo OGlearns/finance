aabab814b78a2d507783429a6eaa7b710a8a5de9
Samuel Ogundare <101474209+OGlearns@users.noreply.github.com>
2023-02-01 11:54:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def sell():
 
         # check to see if they own enough of the stock to sell
 
-        owned_shares = db.execute("SELECT symbol, sum(shares) FROM history WHERE id = %s AND symbol = %s", session["user_id"], symbol))[0]["sum(shares)"]
+        owned_shares = db.execute("SELECT symbol, sum(shares) FROM history WHERE id = $1 AND symbol = $2", session["user_id"], symbol))[0]["sum(shares)"]
 
 #         owned_shares = db.execute("SELECT symbol, sum(shares) FROM history WHERE id, symbol = (%s, %s) GROUP BY symbol", (session["user_id"], symbol))[0]["sum(shares)"]
 
